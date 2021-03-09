884be57c29d4d33fb7fa39fca3b78fc98b759f93
Neil Ramsbottom <nramsbottom@hotmail.com>
2021-03-09 22:02:57 +0100

    Really added a horse this time.

---
+++ Animals/Horse.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace Animals
+{
+    public class Horse : Animal
+    {
+        public override void Eat()
+        {
+            Debug.WriteLine("Horse eats.");
+        }
+
+        public override void Speak()
+        {
+            Debug.WriteLine("Horse speaks.");
+        }
+    }
+}
